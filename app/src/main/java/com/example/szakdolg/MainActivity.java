b365eeafe3f2f42078b82218a9148f3843106946
Szalay Gergely <szalaygergely@gmail.com>
2022-02-08 23:43:11 +0100

    register xml+ chat xml

--- app/src/main/java/com/example/szakdolg/MainActivity.java
+++ app/src/main/java/com/example/szakdolg/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-        Intent intent = new Intent(MainActivity.this, ChatActivity.class);
+        Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
         startActivity(intent);
     }
 }
