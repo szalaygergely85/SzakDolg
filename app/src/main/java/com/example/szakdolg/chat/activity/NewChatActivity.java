77d32c28d224d23a30c3ab494e6f2d0c0fcac989
Gergely Szalay <szalaygergely@gmail.com>
2024-07-07 16:33:04 +0200

    Add NewChatActivity

---
+++ app/src/main/java/com/example/szakdolg/chat/activity/NewChatActivity.java
@@ -0,0 +1,14 @@
+package com.example.szakdolg.chat.activity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.szakdolg.R;
+
+public class NewChatActivity extends AppCompatActivity {
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_new_chat);
+   }
+}
